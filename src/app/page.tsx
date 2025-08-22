0938cf5a2c6d368a8be5ad02bdfa8f9b774d1067
HamidZr <hamidzrdev@gmail.com>
2025-08-22 16:43:51 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import LoginForm from "@/components/auth/LoginForm";
+import AppHeader from "@/components/layout/AppHeader";
+import AppLayout from "@/components/layout/AppLayout";
 
 
 export default function Home() {
   return (
+    <>
+    <AppHeader/>
+    <AppLayout>
 <LoginForm/>
+</AppLayout>
+</>
   );
 }
